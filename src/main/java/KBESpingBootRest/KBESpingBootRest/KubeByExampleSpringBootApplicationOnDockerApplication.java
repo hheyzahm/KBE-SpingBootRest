8464e50712858bb85f25c4bb4f7aa8cbb8fc155f
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-04-03 10:23:48 +0500

    Initial commit

---
+++ src/main/java/KBESpingBootRest/KBESpingBootRest/KubeByExampleSpringBootApplicationOnDockerApplication.java
@@ -0,0 +1,13 @@
+package KBESpingBootRest.KBESpingBootRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeByExampleSpringBootApplicationOnDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeByExampleSpringBootApplicationOnDockerApplication.class, args);
+	}
+
+}
